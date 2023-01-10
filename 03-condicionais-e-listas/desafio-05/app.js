efd188d4e5fea5c41d9b19b092ed907edc26791e
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2023-01-10 09:43:17 -0300

    Seção 3 - Usando condicionais e renderização de listas

---
+++ 03-condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		condicao: true,
+		desafio2:['Monitor', 'Gabinete', 'Processador', 'Mémorias', 'Placa mãe', 'Teclado e mouse']
+	}
+});
