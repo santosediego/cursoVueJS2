94a3b9e132e8cd47ed704d389a71886403af3f1d
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2023-01-12 17:24:50 -0300

    Seção 07 - Introdução aos Componentes

---
+++ 07-componentes/componente-desafio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <Navigation />
+    <Bar />
+    <Carousel />
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Navigation from "@/components/template/Navigation.vue";
+import Bar from "@/components/template/Bar.vue";
+import Carousel from "@/components/widgets/Carousel.vue";
+import Footer from "@/components/template/Footer.vue";
+
+export default {
+  components: {
+    Navigation: Navigation,
+    Bar,
+    Carousel,
+    Footer,
+  }
+};
+</script>
---
+++ 07-componentes/componente-desafio/src/components/template/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-navigation-drawer app>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title"> Menu </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-divider></v-divider>
+
+    <ItensMenu />
+  </v-navigation-drawer>
+</template>
+
+<script>
+
+import ItensMenu from "./ItensMenu.vue";
+
+export default {
+  components: {
+    ItensMenu: ItensMenu,
+  },
+};
+</script>
